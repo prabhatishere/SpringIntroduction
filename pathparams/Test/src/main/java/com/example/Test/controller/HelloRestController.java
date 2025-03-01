e09ae3c51e188119d0937adedf44549b873a6f93
prabhatishere <prabhatkumar14504@gmail.com>
2025-03-01 22:06:35 +0530

    [Prabhat] Add Path Parameters

---
+++ pathparams/Test/src/main/java/com/example/Test/controller/HelloRestController.java
@@ -0,0 +1,30 @@
+package com.example.Test.controller;
+
+
+import org.springframework.web.bind.annotation .*;
+import com.example.Test.model.User;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+
+
+    // curl localhost:8080/hello -w "\n"
+    //uc1
+    @GetMapping(value = {"", "/", "/home"})
+    public String sayHello() {
+        return "Hello From BridgeLabz! ! !";
+
+    }
+
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name){
+        return "Hello " + name+" from Bridgelabz";
+    }
+
+    @GetMapping("/params/{name}")
+    public String sayHelloPathVariable(@PathVariable String name){
+        return "Hello " + name+" from Bridgelabz";
+    }
+
+}
